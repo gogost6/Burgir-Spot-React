6491db32dce3cdb5ff611b21f2640c9d988ccbc6
Georgi Stoilkov <gogost6@gmail.com>
2021-12-26 19:11:53 +0200

    edit err handling update

--- burgir-spot-react/src/components/Burgir/Edit/Edit.js
+++ burgir-spot-react/src/components/Burgir/Edit/Edit.js
@@ -5,13 +5,19 @@ import { burgirDetails, editBurgir } from '../../../services/foodService';
 import { useNavigate } from "react-router";
 import { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
+import * as utils from '../../../utils/styles';
 
 const Edit = () => {
     const params = useParams();
     const { id } = params;
 
     const navigate = useNavigate();
-    const [errors, setErrors] = useState([]);
+
+    const [error, setError] = useState('');
+    let [isSubmitted, setIsSubmitted] = useState(false);
+    let [nameHover, setNameHover] = useState(false);
+    let [priceHover, setPriceHover] = useState(false);
+    let [imgUrlHover, setImgHover] = useState(false);
 
     let [burgir, setBurgir] = useState({
         bonus: [],
@@ -25,6 +31,10 @@ const Edit = () => {
         vegetables: []
     });
 
+    const colorStyles = {
+        control: (styles) => ({ ...styles, 'border': '2px solid red' }),
+    }
+
     useEffect(() => {
         if (id) {
             burgirDetails(id)
@@ -36,28 +46,60 @@ const Edit = () => {
 
     const onSubmit = (e) => {
         e.preventDefault();
-        editBurgir(burgir, id).then(res => navigate('/menu')).catch(err => setErrors(err));
+        setError('');
+        setIsSubmitted(true);
+        editBurgir(burgir, id).then(res => navigate('/menu')).catch(err => setError('Please fill all fields!'));
     }
 
     return (<div className="container wrap">
-        <h1>Edit your burgir!</h1>
+        <h1 className="h1">Edit your burgir!</h1>
         <form method="POST" className="create-form" onSubmit={onSubmit}>
             <div className="form-item-wrapper">
-                <label htmlFor="name">Burger name:</label>
+                <label htmlFor="name">*Burgir name:</label>
                 <input type="text" name="name" id="name" value={burgir.name}
-                    onChange={(e) => changeValue(e, 'name', setBurgir)} />
+                    className="required"
+                    onChange={(e) => changeValue(e, 'name', setBurgir)}
+                    onMouseEnter={() => {
+                        setNameHover(true);
+                    }}
+                    onMouseLeave={() => {
+                        setNameHover(false);
+                    }}
+                    style={{
+                        ...utils.inputBorderStyle.normal,
+                        ...(nameHover ? utils.inputBorderStyle.hover : null),
+                        ...(burgir.name === '' && isSubmitted ? utils.inputBorderStyle.error : null),
+                    }}
+                />
             </div>
             <div className="form-item-wrapper">
-                <label htmlFor="price">Price:</label>
+                <label htmlFor="price">*Price:</label>
                 <input type="text" name="price" id="price" value={burgir.price}
-                    onChange={(e) => changeValue(e, 'price', setBurgir)} />
+                    defaultValue={1}
+                    className="required"
+                    onChange={(e) => changeValue(e, 'price', setBurgir)}
+                    onMouseEnter={() => {
+                        setPriceHover(true);
+                    }}
+                    onMouseLeave={() => {
+                        setPriceHover(false);
+                    }}
+                    style={{
+                        ...utils.inputBorderStyle.normal,
+                        ...(priceHover ? utils.inputBorderStyle.hover : null),
+                        ...(burgir.price == 0 && isSubmitted ? utils.inputBorderStyle.error : null),
+                        ...(burgir.price === '' && isSubmitted ? utils.inputBorderStyle.error : null),
+                    }}
+                />
             </div>
             <div className="form-item-wrapper">
-                <label htmlFor="meat">Meat</label>
+                <label htmlFor="meat">*Meat</label>
                 <Select options={option.meatOptions}
                     name="meat" id="meat"
                     value={[{ value: burgir.meat, label: burgir.meat }]}
-                    onChange={(e) => changeMeatValue(e, setBurgir)} />
+                    onChange={(e) => changeMeatValue(e, setBurgir)}
+                    styles={burgir.meat == '' && isSubmitted ? colorStyles : ''}
+                />
             </div>
             <div className="form-item-wrapper">
                 <label htmlFor="vegetables">Vegetables</label>
@@ -86,17 +128,31 @@ const Edit = () => {
                     onChange={(e) => changeValue(e, 'description', setBurgir)} />
             </div>
             <div className="form-item-wrapper">
-                <label htmlFor="imgUrl">Image Url:</label>
+                <label htmlFor="imgUrl">*Image Url:</label>
                 <input type="text" name="imgUrl" id="imgUrl"
                     value={burgir.imgUrl}
-                    onChange={(e) => changeValue(e, 'imgUrl', setBurgir)} />
+                    onChange={(e) => changeValue(e, 'imgUrl', setBurgir)}
+                    className="required"
+                    onMouseEnter={() => {
+                        setImgHover(true);
+                    }}
+                    onMouseLeave={() => {
+                        setImgHover(false);
+                    }}
+                    style={{
+                        ...utils.inputBorderStyle.normal,
+                        ...(imgUrlHover ? utils.inputBorderStyle.hover : null),
+                        ...(burgir.imgUrl === '' && isSubmitted ? utils.inputBorderStyle.error : null),
+                    }}
+                />
             </div>
             <div className="btn-wrapper-create">
-                <button className="btn burgir-color">Preview</button>
+                {/* <button className="btn burgir-color">Preview</button> */}
                 <button className="btn">Edit</button>
             </div>
         </form>
-        {errors.length > 0 ? <p className="p-err">Opsss you stil have some errors!</p> : ''}
+        <p style={{ 'marginLeft': '200px', 'fontSize': '12px' }}>Inputs with * are required!</p>
+        <p className={error !== '' ? 'p-err hidethis' : ''} style={{ 'opacity': 1, 'display': 'block' }}>{error}</p>
     </div>);
 }
 
