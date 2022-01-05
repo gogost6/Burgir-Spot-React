521d80cf35c191da9576f34fbbf5f05e58ba6e4b
Georgi Stoilkov <gogost6@gmail.com>
2022-01-05 15:23:51 +0200

    wrong old password fix

--- burgir-spot-react/src/components/UserProfile/EditPassword/EditPassword.js
+++ burgir-spot-react/src/components/UserProfile/EditPassword/EditPassword.js
@@ -10,20 +10,24 @@ const EditPassword = () => {
     const dispatch = useDispatch();
 
     const user = useSelector(state => state.user.value);
-    //let { user, setUser, setUserState } = useContext(AuthContext);
 
     let [oldPassword, setOldPassword] = useState('');
     let [newPassword, setNewPassword] = useState('');
     let [isSubmitted, setIsSubmitted] = useState(false);
-    // let [oldPasswordErr, setOldPasswordErr] = useState(false);
+    let [oldPasswordErr, setOldPasswordErr] = useState(false);
 
     const onSubmit = (e) => {
         e.preventDefault();
         setIsSubmitted(true);
+        setOldPasswordErr(false);
         const data = { ...user, oldPassword, newPassword };
         editPasswordHandled(data)
             .then(res => dispatch(userAuthentication(res)))
-            .catch(err => console.log(err));
+            .catch(err => {
+                if(err.msg.includes('Old password is incorrect!')) {
+                    setOldPasswordErr(true);
+                }
+            });
     }
 
     const goBackBtn = () => {
@@ -35,6 +39,8 @@ const EditPassword = () => {
         <input type="password" name="oldPassword" id="oldPassword" onChange={(e) => setOldPassword(e.target.value)} />
         {oldPassword === '' && isSubmitted ? <p className="p-err">Old password should not be empty!</p> : ''}
         {oldPassword.length < 4 && isSubmitted ? <p className="p-err">Old password should be atleast 4 chars!</p> : ''}
+        {oldPasswordErr && isSubmitted ? <p className="p-err">Old password is incorect!</p> : ''}
+
         <label htmlFor="newPassword">New Password</label>
         <input type="password" name="newPassword" id="newPassword" onChange={(e) => setNewPassword(e.target.value)} />
         {oldPassword !== newPassword && isSubmitted ? <p className="p-err">Passwords don't match!</p> : ''}
