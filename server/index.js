aa82a7a30c4d4a2f00eb2448378311f9782ad2de
Georgi Stoilkov <gogost6@gmail.com>
2022-01-16 21:35:11 +0200

    build stuff

--- server/index.js
+++ server/index.js
@@ -16,6 +16,15 @@ app.use(storage());
 
 expressConfig(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+	app.use(express.static(path.join(__dirname, 'burgir-spot-react/build')));
+// Handle React routing, return all requests to React app
+	app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'burgir-spot-react/build', 'index.html'));
+  });
+}
+
 app.listen(port, () =>
   console.log(`Server is running on http://localhost:${port}`)
 );
