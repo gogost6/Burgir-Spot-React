761fb3a4c5daf45f1b8866aab73b2b2fca3ca305
Georgi Stoilkov <gogost6@gmail.com>
2022-01-16 23:10:21 +0200

    update

--- server/config/express.js
+++ server/config/express.js
@@ -8,7 +8,21 @@ const routes = require("./routes");
 
 module.exports = (app) => {
   app.use(express.urlencoded({ extended: false }));
-  app.use(cors(config.CORS));
+  const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://burgir-spot.herokuapp.com/'];
+  const corsOptions = {
+    origin: function (origin, callback) {
+      console.log("** Origin of request " + origin)
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
+        console.log("Origin acceptable")
+        callback(null, true)
+      } else {
+        console.log("Origin rejected")
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+  }
+  app.use(cors(corsOptions))
+  // app.use(cors(config.CORS));
   app.use(cookieParser(config.TOKEN_SECRET));
   app.use(logger());
   app.use(auth());
