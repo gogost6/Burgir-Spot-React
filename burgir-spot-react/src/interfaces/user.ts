1c5c806b67335ec0bf6ff08231b4f29dd21dd98b
Georgi Stoilkov <gogost6@gmail.com>
2022-04-22 20:38:44 +0300

    update

--- burgir-spot-react/src/interfaces/user.ts
+++ burgir-spot-react/src/interfaces/user.ts
@@ -9,3 +9,37 @@ export interface User {
     isLogged: boolean;
     isAdmin: boolean;
 }
+
+export interface RegisterData {
+    email: string;
+    password: string;
+    repassword: string;
+    telephone: string;
+    username: string;
+}
+
+export interface LoginData {
+    username: string;
+    password: string;
+}
+
+export interface EditProfileData {
+    username: string;
+    telephone: string;
+    email:string;
+    oldTelephone: string;
+    oldUsername:string;
+    oldEmail: string;
+}
+
+export interface EditPasswordData {
+    _id: string;
+    email: string;
+    username: string;
+    telephone: string;
+    createdBurgirs: string[];
+    likedBurgirs: string[];
+    isLogged: boolean;
+    oldPassword: string;
+    newPassword: string;
+}
