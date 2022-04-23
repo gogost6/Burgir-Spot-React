441887b6eda47b3790b0082d93758e71d918eea8
Georgi Stoilkov <gogost6@gmail.com>
2022-04-23 19:13:34 +0300

    notfound component migrated

--- burgir-spot-react/src/components/NotFound/NotFound.tsx
+++ burgir-spot-react/src/components/NotFound/NotFound.tsx
@@ -1,15 +1,17 @@
+import CSS from "csstype";
+
 const NotFound = () => {
-    const h1Style = {
+    const h1Style: CSS.Properties = {
         position: "absolute",
         left: "50%",
         top: "50%",
-        transform: "translate(-50%, -50%)"
-    }
+        transform: "translate(-50%, -50%)",
+    };
     return (
         <div className="container">
             <h1 style={h1Style}>Ooops... Page not found!</h1>
         </div>
-    )
-}
+    );
+};
 
 export default NotFound;
